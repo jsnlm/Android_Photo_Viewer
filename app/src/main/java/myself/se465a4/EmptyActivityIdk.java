b96124d4a1286cdd5772f88b98e005c32783e5b7
jsnlm <jasonjslam@gmail.com>
2016-03-25 15:14:12 -0400

    Worked on the Toolbar appearance

---
+++ app/src/main/java/myself/se465a4/EmptyActivityIdk.java
@@ -0,0 +1,13 @@
+package myself.se465a4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivityIdk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_activity_idk);
+    }
+}
